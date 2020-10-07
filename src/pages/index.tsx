e16abced6a07e5d63f8989da30fb0802a67c8912
Muhammad Nouman Atiq <36809880+nomisatti@users.noreply.github.com>
2020-10-08 01:19:10 +0500

    Added functions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,33 @@
-import React from 'react'
-import {graphql, Link} from 'gatsby'
-import {documentToReactComponents} from '@contentful/rich-text-react-renderer'
+import React, { useEffect, useState } from 'react'
+import { graphql, Link } from 'gatsby'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 
-export default ({data}) => {
+export default ({ data }) => {
+
+  const [netlifyData , setNetlifyData] = useState(Object);
+
+  useEffect(() =>{
+    fetch(`/.netlify/functions/hello`)
+    .then(response => response.json())
+    .then(data => setNetlifyData(data));
+}, []);
   console.log(data);
-  const posts = data.allContentfulBlogSite.edges.map((post , index) =>
+  const posts = data.allContentfulBlogSite.edges.map((post, index) =>
     <li key={index}><Link to={post.node.slug}>{post.node.title}</Link></li>
   );
   return (
+    <div>
       <ul>
-          {posts}
+        {posts}
       </ul>
+
+      <hr/>
+      <div>
+        {netlifyData.message}
+      </div>
+
+    </div>
+
   );
 }
 
