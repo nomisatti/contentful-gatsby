21ec737d49eade54ceb2cbbbd80ae9198e22209f
Muhammad Nouman Atiq <36809880+nomisatti@users.noreply.github.com>
2020-11-03 04:08:52 +0500

    Fix dependicies issues

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,45 +2,47 @@ import React from 'react'
 import { Link } from 'gatsby'
 import NomiSatti from '../images/NomiSatti.jpg'
 
-const Sidebar = ({ data }) => {
 
+const Sidebar = ({ data }) => {
+ 
 
     console.log("SideBar : ", data)
     return (
-        <>
+        <>    
 
             <div className="w3-card w3-margin w3-margin-top">
                 <img src={NomiSatti} />
                 <div className="w3-container w3-white">
                     <h4><b>M Nouman Atiq</b></h4>
                     <p>Software Developer</p>
-                    <a href="https://www.facebook.com/noumasatti39/" className="fa fa-facebook"></a>   
-                    <a href="https://github.com/nomisatti" className="fa fa-github"></a>                
-                    <a href="https://www.linkedin.com/in/noumanatiq/" className="fa fa-linkedin"></a>
-                   
+
+                    <a href="https://www.facebook.com/noumasatti39/" className="fa fa-facebook">F</a>
+                    <a href="https://github.com/nomisatti" className="fa fa-github">G</a>
+                    <a href="https://www.linkedin.com/in/noumanatiq/" className="fa fa-linkedin">L</a>
+
                 </div>
             </div><hr />
-            {data.allContentfulBlogSite ? 
-             (
-                <div className="w3-card w3-margin">
-                <div className="w3-container w3-padding">
-                    <h4>Recent Posts</h4>
-                </div>
-                <ul className="w3-ul w3-hoverable w3-white">
-                    {data.allContentfulBlogSite.edges.map((post, index) =>
-                        <li key={index} className="w3-padding-16">
-                            <img src={post.node.image.file.url} alt={post.node.image.title} className="w3-left w3-margin-right" />
-                            <span className="w3-large"><Link to={post.node.slug}>{post.node.title}</Link></span><br />
-                            
-                        </li>
-                    )}
-                   
-                </ul>
-            </div>
+            {data.allContentfulBlogSite ?
+                (
+                    <div className="w3-card w3-margin">
+                        <div className="w3-container w3-padding">
+                            <h4>Recent Posts</h4>
+                        </div>
+                        <ul className="w3-ul w3-hoverable w3-white">
+                            {data.allContentfulBlogSite.edges.map((post, index) =>
+                                <li key={index} className="w3-padding-16">
+                                    <img src={post.node.image.file.url} alt={post.node.image.title} className="w3-left w3-margin-right" />
+                                    <span className="w3-large"><Link to={post.node.slug}>{post.node.title}</Link></span><br />
 
-             ) : null
+                                </li>
+                            )}
+
+                        </ul>
+                    </div>
+
+                ) : null
             }
-           
+
         </>
     )
 }
